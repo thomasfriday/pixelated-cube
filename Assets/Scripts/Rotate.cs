44fee3a81ab3cf0825fb98be789f9a1f7db18442
Thomas Friday <85455452+thomasfriday@users.noreply.github.com>
2021-06-07 17:02:25 +0200

    first commit

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 1f;
+
+    void Update()
+    {
+        Vector3 angle = new Vector3(0, 45, 0);
+        transform.Rotate(angle * Time.deltaTime * speed);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# How to Code an Exploding Cube in Unity 
+
+[Watch the video tutorial on Youtube](https://youtu.be/Sru8XDwxC3I).
